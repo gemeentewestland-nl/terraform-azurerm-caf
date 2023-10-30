2512a25477aca189a2dccb004fa3a531879ceba3
jvdhaterdwestland <134271508+jvdhaterdwestland@users.noreply.github.com>
2023-10-30 13:51:14 +0000

    container app job

---
+++ modules/compute/container_app_job/container_app_job.tf
@@ -0,0 +1,94 @@
+resource "azurecaf_name" "ca" {
+  name          = var.settings.name
+  prefixes      = var.global_settings.prefixes
+  resource_type = "azurerm_container_app"
+  random_length = var.global_settings.random_length
+  clean_input   = true
+  passthrough   = var.global_settings.passthrough
+  use_slug      = var.global_settings.use_slug
+}
+
+data "azurerm_resource_group" "ca" {
+  name = local.resource_group_name
+}
+
+resource "azapi_resource" "container_app_job" {
+  type      = "Microsoft.App/jobs@2023-05-02-preview"
+  name      = azurecaf_name.ca.result
+  location  = data.azurerm_resource_group.ca.location 
+  parent_id = data.azurerm_resource_group.ca.id
+  tags = merge(local.tags, try(var.settings.tags, {}))
+
+  dynamic "identity" {
+    for_each = can(var.settings.identity) ? [1] :[] 
+    
+    content {
+      type         = var.settings.identity.type
+      identity_ids = local.managed_identities
+    }
+  }
+
+  # Need to set to false because at the moment only 2022-11-01-preview is supported
+  schema_validation_enabled = false
+  ignore_missing_property = true
+  ignore_casing = true
+
+  body = jsonencode({
+    properties = {
+      
+      environmentId       = var.combined_objects.container_app_environments[try(var.settings.container_app_environment.lz_key, var.client_config.landingzone_key)][try(var.settings.container_app_environment_key, var.settings.container_app_environment.key)].id
+      workloadProfileName = try(var.settings.workload_profile_name, "Consumption")
+      configuration = {
+        dapr                  = can(var.settings.dapr) ? {
+          appId = var.settings.dapr.id
+          appPort = try(var.settings.dapr.port, null)
+          appProtocol = try(var.settings.dapr.protocol, null)
+          enableApiLogging = try(var.settings.dapr.enable_api_logging, null)
+          enabled = try(var.settings.dapr.enabled, null)
+          httpMaxRequestSize = try(var.settings.dapr.http_max_request_size, null)
+          httpReadBufferSize = try(var.settings.dapr.http_read_buffer_size, null)
+          logLevel = try(var.settings.dapr.log_level, null)
+        } :null 
+
+        secrets               = try(local.secrets, null)
+        triggerType           = var.settings.trigger_type
+        replicaTimeout        = try(var.settings.replica_timeout, 10800) # 3 hours
+        replicaRetryLimit     = try(var.settings.replica_retry_limit, 1)
+        manualTriggerConfig   = var.settings.trigger_type == "Manual" ? {
+          parallelism            = try(var.settings.parallelism, 1)
+          replicaCompletionCount = try(var.settings.replica_completion_count, 1)
+        }:null
+        eventTriggerConfig    = var.settings.trigger_type == "Event" ? {
+          replicaCompletionCount = try(var.settings.replica_completion_count, 1)
+          parallelism            = try(var.settings.parallelism, 1)
+          scale = {
+            minExecutions   = try(var.settings.scale_min_executions, 0)
+            maxExecutions   = try(var.settings.scale_max_executions, 10)
+            pollingInterval = try(var.settings.scale_polling_interval, 30)
+            rules           = local.job_scale_rules
+          }
+        }: null
+        scheduleTriggerConfig = var.settings.trigger_type == "Schedule" ? {
+          cronExpression         = var.settings.cron_expression
+          replicaCompletionCount = try(var.settings.replica_completion_count, 1)
+          parallelism            = try(var.settings.parallelism, 1)
+        }:null
+        
+        registries = can(var.settings.registry) ? [
+          {
+            server = var.settings.registry.server
+            identity = try(var.settings.registry.identity, null)
+            passwordSecretRef = try(var.settings.registry.password_secret_name, null)    
+            username = try(var.settings.registry.username, null)
+          } 
+        ] : []    
+      }
+      template = {
+        containers = local.containers
+        
+        initContainers = can(var.settings.init_containers) ? local.init_containers : null
+        volumes        = try(local.volumes, null)
+      }
+    }
+  })
+}
---
+++ modules/compute/container_app_job/locals.tf
@@ -0,0 +1,192 @@
+locals {
+  secrets = flatten([
+    for secret in try(var.settings.secrets, []) : [
+      {
+        name          = secret.name
+        value         = try(secret.value, null)
+        identity      = try(try(secret.managed_identity_id, var.combined_objects.managed_identities[try(secret.managed_identity.lz_key, var.client_config.landingzone_key)][try(secret.managed_identity_key, secret.managed_identity.key)].id), null)
+        keyvault_url  = try(
+          try(
+            secret.keyvault_url, 
+            format("%ssecrets/%s", var.combined_objects.keyvaults[try(secret.keyvault.lz_key, var.client_config.landingzone_key)][try(secret.keyvault_key, secret.keyvault.key)].vault_uri, try(var.dynamic_keyvault_secrets[try(secret.keyvault_key, secret.keyvault.key)][secret.dynamic_keyvault_secret_key].secret_name, try(secret.keyvault_secret_name, null)))
+          )
+        , null)
+      }
+    ] if can(secret.value) || can(try(secret.managed_identity_id, try(secret.managed_identity_key, secret.managed_identity.key)))
+  ])
+
+  dynamic_secrets = flatten([
+    for secret in try(var.settings.secrets, []) : [
+      {
+        name                  = secret.name
+        keyvault_secret_name  = try(var.dynamic_keyvault_secrets[try(secret.keyvault_key, secret.keyvault.key)][secret.dynamic_keyvault_secret_key].secret_name, secret.keyvault_secret_name)
+        key_vault_id          = try(secret.key_vault_id, try(var.combined_objects.keyvaults[try(secret.keyvault.lz_key, var.client_config.landingzone_key)][try(secret.keyvault_key, secret.keyvault.key)].id, null))
+      }
+    ] if (can(secret.dynamic_keyvault_secret_key) || can(secret.keyvault_secret_name)) && !(can(try(secret.keyvault_url, secret.identity)))
+  ])
+
+  managed_local_identities = flatten([
+    for managed_identity_key in try(var.settings.identity.managed_identity_keys, []) : [
+      var.combined_resources.managed_identities[var.client_config.landingzone_key][managed_identity_key].id
+    ]
+  ])
+
+  managed_remote_identities = flatten([
+    for lz_key, value in try(var.settings.identity.remote, []) : [
+      for managed_identity_key in value.managed_identity_keys : [
+        var.combined_resources.managed_identities[lz_key][managed_identity_key].id
+      ]
+    ]
+  ])
+
+  managed_identities = concat(local.managed_local_identities, local.managed_remote_identities)
+
+  job_scale_rules = flatten([
+    for rule in try(var.settings.event_scale_rule, []): [
+      {
+        name = rule.name
+        type = rule.job_rule_type
+        metadata = rule.metadata
+        auth = flatten([
+          for auth in try(rule.authentication,[]): [
+            {
+              secretRef = auth.secret_name
+              triggerParameter = auth.trigger_parameter
+            }
+          ]
+        ])
+      }
+    ]
+  ])
+  
+  containers = flatten([
+    for key, container in var.settings.containers: [{
+      image   = container.image
+      name    = try(container.name, "${key}")
+      command = try(container.command, null)
+      args    = try(container.args, null)
+      env     = try(container.env, null)
+      probes = concat(
+        flatten([
+          for spk, startup_probe in try(container.startup_probe,[]): [{
+            type                             = "Startup"
+            failureThreshold                 = try(startup_probe.failure_count_threshold, null)
+            httpGet = startup_probe.transport != "TCP" ? {
+              httpHeaders = flatten([
+                for hk, header in try(startup_probe.header, []): [{
+                  name = header.name
+                  value = header.value
+                }]
+              ])
+              host                           = try(startup_probe.host, null)
+              path                           = try(startup_probe.path, null)
+              port                           = startup_probe.port
+              scheme                         = startup_probe.transport
+            }: null
+            tcpSocket = startup_probe.transport == "TCP" ? {
+              host                       = try(startup_probe.host, null)
+              port                       = startup_probe.port
+            }: null
+            periodSeconds                    = try(startup_probe.interval_seconds, null)    
+            timeoutSeconds                   = try(startup_probe.timeout, null)
+            terminationGracePeriodSeconds	   = try(startup_probe.termination_grace_period_seconds, null)
+          }]
+        ]),
+        flatten([
+          for lpk, liveness_probe in try(container.liveness_probe,[]): [{
+            type                             = "Liveness"
+            failureThreshold                 = try(liveness_probe.failure_count_threshold, null)
+            httpGet = liveness_probe.transport != "TCP" ? {
+              httpHeaders = flatten([
+                for hk, header in try(liveness_probe.header, []): [{
+                  name = header.name
+                  value = header.value
+                }]
+              ])
+              host                           = try(liveness_probe.host, null)
+              path                           = try(liveness_probe.path, null)     
+              port                           = liveness_probe.port
+              scheme                         = liveness_probe.transport
+            }: null
+            initialDelaySeconds = try(liveness_probe.initial_delay, null)
+            tcpSocket = liveness_probe.transport == "TCP" ? {
+              host                       = try(liveness_probe.host, null)
+              port                       = liveness_probe.port
+            }: null
+            periodSeconds                    = try(liveness_probe.interval_seconds, null)    
+            timeoutSeconds                   = try(liveness_probe.timeout, null)
+            terminationGracePeriodSeconds	   = try(liveness_probe.termination_grace_period_seconds, null)
+          }]
+        ]),
+        flatten([
+          for rpk, readiness_probe in try(container.readiness_probe,[]): [{
+            type                             = "Readiness"
+            failureThreshold                 = try(readiness_probe.failure_count_threshold, null)
+            httpGet = readiness_probe.transport != "TCP" ? {
+              httpHeaders = flatten([
+                for hk, header in try(readiness_probe.header, []): [{
+                  name = header.name
+                  value = header.value
+                }] if readiness_probe.transport != "TCP"
+              ])
+              host                           = try(readiness_probe.host, null)
+              path                           = try(readiness_probe.path, null)     
+              port                           = readiness_probe.port
+              scheme                         = readiness_probe.transport
+            }: null
+            successThreshold = try(readiness_probe.success_count_threshold, null)
+            tcpSocket = readiness_probe.transport == "TCP" ? {
+              host                       = try(readiness_probe.host, null)
+              port                       = readiness_probe.port
+            }: null
+            periodSeconds                    = try(readiness_probe.interval_seconds, null)    
+            timeoutSeconds                   = try(readiness_probe.timeout, null)
+            terminationGracePeriodSeconds	   = try(readiness_probe.termination_grace_period_seconds, null)
+          }]
+        ])
+      )
+      resources = {
+        cpu    = container.cpu
+        memory = container.memory
+      }
+      volumeMounts = flatten([
+        for vmk, volume_mount in try(container.volume_mounts,[]): [{
+          mountPath  = volume_mount.path
+          volumeName = volume_mount.name
+          subPath    = try(volume_mount.sub_path, null)
+        }]
+      ])
+    }]
+  ])
+
+  init_containers = flatten([
+    for key, container in try(var.settings.init_containers, []): [{
+      image   = container.image
+      name    = try(container.name, "${key}")
+      command = try(container.command, null)
+      args    = try(container.args, null)
+      env     = try(container.env, null)
+      resources = {
+        cpu    = container.cpu
+        memory = container.memory
+      }
+      volumeMounts = flatten([
+        for vmk, volume_mount in try(container.volume_mounts,[]): [{
+          mountPath  = volume_mount.path
+          volumeName = volume_mount.name
+          subPath    = try(volume_mount.sub_path, null)
+        }]
+      ])
+    }]
+  ])
+
+  volumes = flatten([
+    for vk, volume in try(var.settings.volumes, []): [{
+      mountOptions = try(volume.mount_options, null)
+      name         = volume.name
+      secrets      = try(volume.secrets, null)
+      storageName  = try(volume.storage_name, null)
+      storageType  = try(volume.storage_type, "EmptyDir")
+    }]
+  ])
+}
