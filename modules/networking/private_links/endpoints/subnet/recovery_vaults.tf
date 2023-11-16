75cc68baed400a256c9e6f0a7d0323e262c8bcde
marcomansiwestland <134270083+marcomansiwestland@users.noreply.github.com>
2023-11-16 12:55:24 +0100

    try this

--- modules/networking/private_links/endpoints/subnet/recovery_vaults.tf
+++ modules/networking/private_links/endpoints/subnet/recovery_vaults.tf
@@ -28,7 +28,8 @@ module "recovery_vault_remote" {
   location            = var.vnet_location # The private endpoint must be deployed in the same region as the virtual network.
   name                = try(each.value.name, each.key)
   private_dns         = var.private_dns
-  resource_group_name = try(var.resource_groups[each.value.resource_group_key].name, var.vnet_resource_group_name)
+  #resource_group_name = try(var.resource_groups[each.value.resource_group_key].name, var.vnet_resource_group_name)
+  resource_group_name = var.resource_groups[each.value.resource_group_key].name
   resource_id         = var.remote_objects.recovery_vaults[each.value.lz_key][each.key].id
   settings            = each.value
   subnet_id           = var.subnet_id
