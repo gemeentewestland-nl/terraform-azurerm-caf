2c07f775884e1b7b71ae47ee678efd5b3e8472f7
Frank <133880060+FrankGNL@users.noreply.github.com>
2025-04-07 10:30:30 +0200

    update

--- modules/azuread/applications_v1/azuread_application.tf
+++ modules/azuread/applications_v1/azuread_application.tf
@@ -140,7 +140,7 @@ resource "random_uuid" "oauth2_permission_scopes" {
 }
 
 resource "azuread_application_federated_identity_credential" "federeded_credentials" {
-  for_each = var.settings.federeded_credentials != null && length(var.settings.federeded_credentials) > 0 ? var.settings.federeded_credentials : {}
+  for_each = length(var.settings.federeded_credentials) > 0 ? var.settings.federeded_credentials : []
 
   application_object_id = azuread_application.app.id
   display_name          = each.value.display_name
